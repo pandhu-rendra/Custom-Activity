f5866249a4f367c77f64f3f78ba6cd111575648b
ItsFexnes <133106820+ItsFexnes@users.noreply.github.com>
2023-06-18 22:37:21 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const server = express();
- 
-server.all('/', (req, res) => {
-  res.send(`NOW GO TO ANY UPTIME APP and paste this link`)
+
+server.all('/', (req, res,) => {
+    res.send('Copy the url or link from webview and open https://uptimerobot.com/ create a new website there and paste the link from webview')
 })
- 
+
 function keepAlive() {
-  server.listen(3000, () => { console.log("Server is Ready!!" + Date.now()) });
+    server.listen(3000, () => { console.log("Server is ready!!" + Date.now()) });
 }
- 
+
 module.exports = keepAlive;
